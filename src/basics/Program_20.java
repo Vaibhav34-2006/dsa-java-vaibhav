dd8f7ebb811637fcb21107a3a1156d045315d83c
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 15:03:26 +0530

    Check if a student is pass or fail using ternary operator.

---
+++ src/basics/Program_20.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Program_20 {
+
+	public static void main(String[] args) {
+		int marks = 35;
+		String result = (marks >= 40) ? "Pass" : "Fail";
+		System.out.println(result);
+
+
+	}
+
+}
