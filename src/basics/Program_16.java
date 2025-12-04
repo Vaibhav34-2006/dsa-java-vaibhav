827d70d37cbdbc863db4907c95f090766bd312d5
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 14:34:41 +0530

    if- else if- else

---
+++ src/basics/Program_16.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class Program_16 {
+
+	public static void main(String[] args) {
+		int age = 20;
+		if(age >= 18) {
+			System.out.println("Adult");
+		} else if(age >= 13 && age < 18) {
+			System.out.println("Teenager");
+		} else {
+			System.out.println("Child");
+		}
+
+	}
+
+}
