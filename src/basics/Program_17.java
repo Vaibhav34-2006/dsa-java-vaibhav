9e927d6f6df8371f17e8daaa0ebca2ceb2a377e0
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 14:45:36 +0530

    Income tax calculator.

---
+++ src/basics/Program_17.java
@@ -0,0 +1,19 @@
+package basics;
+import java.util.*;
+public class Program_17 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int income = sc.nextInt();
+		int tax;
+		if(income < 500000) {
+			tax =0;
+		}else if (income >= 500000 && income < 1000000) {
+			tax = (int) (income * 0.2);
+		}else {
+			tax = (int) (income * 0.3);
+		}
+
+	}
+
+}
