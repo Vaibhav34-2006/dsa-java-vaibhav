3f97e89fc51077fcc749924457be8df039fb9488
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 09:11:46 +0530

    Variables in java

---
+++ src/basics/Program_3.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Program_3 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		System.out.println(a);
+		System.out.println(b);
+
+		String name = "Tony";
+		System.out.println(name);
+		
+		a=b;
+		System.out.println(a);
+		
+	}
+
+}
