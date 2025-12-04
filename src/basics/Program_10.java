a755dea27ee1a8f72a9d014897bbc7b5ff7cfb20
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 11:29:23 +0530

    Relational Operators.

---
+++ src/basics/Program_10.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class Program_10 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 5;
+
+		System.out.println(a == b);
+		System.out.println(a != b);
+		System.out.println(a > b);
+		System.out.println(a < b);
+		System.out.println(a >= b);
+		System.out.println(a <= b);
+
+	}
+
+}
