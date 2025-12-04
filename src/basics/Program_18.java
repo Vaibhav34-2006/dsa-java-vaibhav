93acbf1695e0a9a3fbf8fba225efa0fd405b2af4
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 14:53:25 +0530

    Print the largest of 3 number.

---
+++ src/basics/Program_18.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Program_18 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 4;
+		int c = 20;
+		if((a>=b) && (a>=c)) {
+			System.out.println("A is the largest number");
+		}else if (b>=c) {
+			System.out.println("B is the largest number");
+		}else {
+			System.out.println("C is the largest number");
+		}
+
+	}
+
+}
