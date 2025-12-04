e03a21b30626ec5c046d4755d05770c970b69ecc
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 09:54:31 +0530

    Area of circle.

---
+++ src/basics/Program_7.java
@@ -0,0 +1,14 @@
+package basics;
+import java.util.*;
+public class Program_7 {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the value of r: ");
+	int r = sc.nextInt();
+	float circle = (float) (3.14 * r*r);
+	System.out.println(circle);
+	
+	}
+
+}
