4bd90afa758b780f16ac208808b2b2f03a8acb96
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 09:47:15 +0530

    Sum of two number from the user.

---
+++ src/basics/Program_6.java
@@ -0,0 +1,19 @@
+package basics;
+import java.util.*;
+
+public class Program_6 {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter the value of a: ");
+         int a = sc.nextInt();
+
+		 System.out.println("Enter the value of b: ");
+         int b = sc.nextInt();
+         int sum = a+b;
+         
+         System.out.println(sum);
+		
+	}
+
+}
