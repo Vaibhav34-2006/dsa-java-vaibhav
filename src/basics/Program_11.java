a72c460edd919e791da31829f747758502272771
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 11:34:27 +0530

    Logical Operators.

---
+++ src/basics/Program_11.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Program_11 {
+
+	public static void main(String[] args) {
+		    boolean a = true;
+	        boolean b = false;
+
+	        System.out.println(a && b);
+	        System.out.println(a || b);
+	        System.out.println(!a);
+
+	}
+
+}
