fa3698db8c9bcd4403dc310382d730fb08813841
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 08:53:22 +0530

    Print the pattern using println function.

---
+++ src/basics/Program_2.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class Program_2 {
+
+	public static void main(String[] args) {
+		System.out.println("* * * * *");
+		System.out.println("* * * *");
+		System.out.println("* * *");
+		System.out.println("* *");
+		System.out.println("*");
+
+	}
+
+}
