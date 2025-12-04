0deb4d4d2218c5acc55942d45235f967ba4aa3d7
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 14:15:21 +0530

    If else problem.

---
+++ src/basics/Program_13.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class Program_13 {
+
+	public static void main(String[] args) {
+		int age = 18;
+		if(age>=18) {
+		System.out.println("Adult");
+		}
+		else {
+			System.out.println("Not Adult");
+		}
+
+	}
+
+}
