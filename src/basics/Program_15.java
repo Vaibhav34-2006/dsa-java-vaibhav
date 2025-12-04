4a48d01f66b0616657be1d7a9ac8d5614102d87f
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 14:29:12 +0530

    Print the no. is odd or even.

---
+++ src/basics/Program_15.java
@@ -0,0 +1,14 @@
+package basics;
+import java.util.*;
+public class Program_15 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		if(a % 2 == 0) {
+			System.out.println("EVEN");
+		}else {
+			System.out.println("ODD");
+		}
+	}
+}
