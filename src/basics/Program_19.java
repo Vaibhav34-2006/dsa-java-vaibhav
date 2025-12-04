52a1bd1fe3bda146ee5bf4a5fe64dd9beed7b879
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 14:59:17 +0530

    Ternary operator.

---
+++ src/basics/Program_19.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class Program_19 {
+
+	public static void main(String[] args) {
+		int a = 4;
+		String type = ((a % 2)==0)?"even":"odd";
+		System.out.println(type);
+	}
+}
