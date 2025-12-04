45faa32deeb71cd8ec8a275acc0f2a7f51d09f65
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 14:20:16 +0530

    Print the largest of 2 numbers.

---
+++ src/basics/Program_14.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class Program_14 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		if(a>b) {
+			System.out.println("A is the largest number");
+		}
+		else {
+			System.out.println("B is the largest number");
+		}
+
+	}
+
+}
