cb50bca8aac24812de3d4253f2f63591f3d679ed
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-04 09:25:27 +0530

    Arithmetic Operations.

---
+++ src/basics/Program_4.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Program_4 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int sum = a+b;
+		int sub = a-b;
+		int mul = a*b;
+		int div = a/b;
+		System.out.println(sum);
+		System.out.println(sub);
+		System.out.println(mul);
+		System.out.println(div);
+
+	}
+
+}
