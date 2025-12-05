198e27b2c0d0055ac984025a052571321cbcf076
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-05 09:23:25 +0530

    Basic loop question

---
+++ src/basics/Program_22.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class Program_22 {
+
+	public static void main(String[] args) {
+		int a = 0;
+		while (a < 10) {
+			System.out.println("Hello World");
+			a++;
+		}
+        System.out.println("Loop completed");
+	}
+
+}
