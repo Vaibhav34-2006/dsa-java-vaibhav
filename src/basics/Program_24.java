c7ee84edfef4b7ea06e1c47ed13579f88fb69822
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-05 09:36:09 +0530

    Print the no. form 1 to n.

---
+++ src/basics/Program_24.java
@@ -0,0 +1,16 @@
+package basics;
+import java.util.*;
+public class Program_24 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a = 1;
+		while (a <= n) {
+			System.out.println(a);
+			a++;
+		}
+
+	}
+
+}
