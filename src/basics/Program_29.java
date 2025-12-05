0b95d34fe2a17846f4862eb606c8704a281964a6
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-05 15:52:32 +0530

    Break Statement.

---
+++ src/basics/Program_29.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Program_29 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=5;i++) {
+			if(i==3) {
+			System.out.println(i);
+			}
+			System.out.println("I am out of loop");
+		}
+
+	}
+
+}
