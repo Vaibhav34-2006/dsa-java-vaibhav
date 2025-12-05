c81eba3fbae81f0e2240f46b92830e2e5407902a
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-05 16:13:55 +0530

    Continue Statement. To skip an iteration.

---
+++ src/basics/Program_31.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Program_31 {
+
+	public static void main(String[] args) {
+		for(int i=1 ; i<=5 ; i++) {
+			if(i == 3) {
+				continue;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
