518f3f60b8bf1dc2ac25fe3b5a8d1088a94babcb
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-06 19:16:01 +0530

    zero-one-triangle.

---
+++ src/basics/Program_55.java
@@ -0,0 +1,23 @@
+package basics;
+
+public class Program_55 {
+
+    public static void main(String[] args) {
+
+        int n = 5; // you can change this
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
