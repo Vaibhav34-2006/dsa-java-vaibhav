da9ecd5c82e2ebb53e58b32d9a4ba51064ac2c60
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-06 10:45:15 +0530

    Functions basic eg.

---
+++ src/basics/Program_37.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Program_37 {
+	public static void printhw() {
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD");
+	}
+
+	public static void main(String[] args) {
+		printhw();
+		
+	}
+
+}
