b587d18f8e3ffe900795c85b2c5780f7ec2fc074
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-06 11:04:24 +0530

    Sum of digit using function.

---
+++ src/basics/Program_38.java
@@ -0,0 +1,24 @@
+package basics;
+import java.util.*;
+public class Program_38 {
+	public static void printhw() {
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD");
+	}
+	
+	public static void sum() {
+		Scanner sc = new Scanner (System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int sum = a+b;
+		System.out.println(sum);
+		
+	}
+
+	public static void main(String[] args) {
+		printhw();
+		sum();	
+	}
+
+}
