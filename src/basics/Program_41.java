fc4d0d0008fe84ab4a2bf67a4b8c715b2d72cf4f
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-06 11:44:15 +0530

    Product of a & b using function.

---
+++ src/basics/Program_41.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class Program_41 {
+    public static void mul() {
+    	int a=5;
+    	int b=8;
+    	int mul = a*b;
+    	System.out.println(mul);
+    	
+    }
+	public static void main(String[] args) {
+	     mul();
+
+	}
+
+}
