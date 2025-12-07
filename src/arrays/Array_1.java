54ddf98ceb119a652fe096d4eb099b2e9081cf78
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-07 10:48:46 +0530

    Creating and Array.

---
+++ src/arrays/Array_1.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class Array_1 {
+
+	public static void main(String[] args) {
+		// 1. Creating array with size
+        int arr1[] = new int[3];
+
+        // 2. Creating array with values
+        int arr2[] = {10, 20, 30};
+	}
+
+}
