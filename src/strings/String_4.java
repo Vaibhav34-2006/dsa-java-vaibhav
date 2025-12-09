9b0e25731583290346bc14c386d52d1a6dbd8330
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-09 11:14:52 +0530

    String Concatenation.

---
+++ src/strings/String_4.java
@@ -0,0 +1,19 @@
+package strings;
+
+public class String_4 {
+
+    public static void main(String[] args) {
+
+        // Creating first name string
+        String firstname = "Vaibhav";
+
+        // Creating last name string
+        String lastname = "Mali";
+
+        // Concatenating (joining) first name + last name
+        String fullname = firstname + lastname;
+
+        // Printing the full name
+        System.out.println(fullname);
+    }
+}
