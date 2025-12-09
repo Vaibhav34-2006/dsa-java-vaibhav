d9fe20e7a40d5bb7cba84bfb1df396aee6636c92
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-09 10:51:13 +0530

    Added basic string initialization examples with comments.

---
+++ src/strings/String_1.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class String_1 {
+
+    public static void main(String[] args) {
+
+        // Creating a character array 
+        int arr[] = {'a', 'b', 'c', 'd'};  
+       
+        // Creating a string directly 
+        String str = "abcd";
+
+        // Creating a string using new keyword 
+        String str2 = new String("xyz");
+    }
+}
