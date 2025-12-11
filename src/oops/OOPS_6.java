d04507f98463fb9b67eb0ff33d8d0614c2a6a2af
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-11 10:17:34 +0530

    non-parameterized constructor

---
+++ src/oops/OOPS_6.java
@@ -0,0 +1,14 @@
+package oops;
+
+class Car {
+
+    Car() {                // non-parameterized constructor
+        System.out.println("Car object created");
+    }
+}
+
+public class OOPS_6 {
+    public static void main(String[] args) {
+        Car c = new Car(); // constructor runs here
+    }
+}
