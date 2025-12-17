19199ba4b64ae6c16773d777b46604a4fe6fff8b
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-17 10:46:59 +0530

    To find the minimum element

--- src/arraylist/Arraylist_6.java
+++ src/arraylist/Arraylist_6.java
@@ -1,10 +1,32 @@
 package arraylist;
 
+import java.util.ArrayList;
+
 public class Arraylist_6 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        // Create an ArrayList to store Integer values
+        ArrayList<Integer> list = new ArrayList<>();
 
+        // Add elements to the ArrayList
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        // Initialize min with the maximum possible integer value
+        int min = Integer.MAX_VALUE;
+
+        // Loop through the ArrayList to find the minimum element
+        for (int i = 0; i < list.size(); i++) {
+
+            // If current element is smaller than min, update min
+            if (min > list.get(i)) {
+                min = list.get(i);
+            }
+        }
+
+        // Print the minimum value
+        System.out.println("Minimum element is: " + min);
+    }
 }
