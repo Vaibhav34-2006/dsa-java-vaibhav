6b19496dd9db7870f940df1db9f389919a9bf158
Vaibhav Mali <malivaibhav162@gmail.com>
2025-12-17 09:22:45 +0530

    Initialize ArrayList with different data types

---
+++ src/arraylist/Arraylist_1.java
@@ -0,0 +1,24 @@
+package arraylist;
+import java.util.*;
+
+public class Arraylist_1 {
+
+    public static void main(String[] args) {
+
+        // Integer type ArrayList
+        ArrayList<Integer> intList = new ArrayList<>();
+
+        // String type ArrayList
+        ArrayList<String> stringList = new ArrayList<>();
+
+        // Double type ArrayList
+        ArrayList<Double> doubleList = new ArrayList<>();
+
+        // Boolean type ArrayList
+        ArrayList<Boolean> booleanList = new ArrayList<>();
+
+        // Character type ArrayList
+        ArrayList<Character> charList = new ArrayList<>();
+
+    }
+}
