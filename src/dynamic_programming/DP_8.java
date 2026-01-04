e88e628f215db80722c28749ab365899b6c6900c
Vaibhav Mali <malivaibhav162@gmail.com>
2026-01-04 16:46:08 +0530

    0/1 Knapsack using Tabulation

--- src/dynamic_programming/DP_8.java
+++ src/dynamic_programming/DP_8.java
@@ -2,83 +2,54 @@ package dynamic_programming;
 
 public class DP_8 {
 
-    // Function to print the DP table
-    public static void print(int dp[][]) {
-
-        // Loop through rows
-        for (int i = 0; i < dp.length; i++) {
-
-            // Loop through columns
-            for (int j = 0; j < dp[0].length; j++) {
-
-                // Print each cell with space
-                System.out.print(dp[i][j] + " ");
-            }
-
-            // Move to next line after each row
-            System.out.println();
-        }
-
-        // Extra line for readability
-        System.out.println();
-    }
-
-    // 0/1 Knapsack using TABULATION
-    public static int knapsackTab(int val[], int wt[], int W) {
+    // 0/1 Knapsack using Tabulation (Bottom-Up DP)
+    // Each item can be taken ONLY ONCE
+    public static int knapsack01(int val[], int wt[], int W) {
 
         // Number of items
         int n = val.length;
 
-        // DP table where
-        // dp[i][j] = max profit using first i items with capacity j
+        // DP table
+        // dp[i][j] = maximum profit using first i items
+        // with knapsack capacity j
         int dp[][] = new int[n + 1][W + 1];
 
         // Initialize 0th column
-        // If capacity is 0, profit = 0
-        for (int i = 0; i < dp.length; i++) { // 0th column
+        // If capacity is 0, profit is always 0
+        for (int i = 0; i < n + 1; i++) {
             dp[i][0] = 0;
         }
 
         // Initialize 0th row
-        // If no items, profit = 0
-        for (int j = 0; j < dp[0].length; j++) { // 0th row
+        // If there are 0 items, profit is always 0
+        for (int j = 0; j < W + 1; j++) {
             dp[0][j] = 0;
         }
 
         // Fill DP table
-        for (int i = 1; i < n + 1; i++) {          // items loop
-            for (int j = 1; j < W + 1; j++) {      // capacity loop
+        for (int i = 1; i < n + 1; i++) {        // items loop
+            for (int j = 1; j < W + 1; j++) {    // capacity loop
 
-                // Value of ith item
-                int v = val[i - 1];
+                // If current item weight is within capacity
+                if (wt[i - 1] <= j) {   // valid case
 
-                // Weight of ith item
-                int w = wt[i - 1];
+                    // Include the item (move to previous item → 0/1 rule)
+                    int include = val[i - 1] + dp[i - 1][j - wt[i - 1]];
 
-                // If item weight is within capacity
-                if (w <= j) { // valid
+                    // Exclude the item
+                    int exclude = dp[i - 1][j];
 
-                    // Include item
-                    int incProfit = v + dp[i - 1][j - w];
-
-                    // Exclude item
-                    int excProfit = dp[i - 1][j];
-
-                    // Store maximum profit
-                    dp[i][j] = Math.max(incProfit, excProfit);
+                    // Take maximum of include and exclude
+                    dp[i][j] = Math.max(include, exclude);
                 }
                 // If item weight is more than capacity
-                else { // invalid
+                else {   // invalid case
 
-                    // Only exclude item
-                    int excProfit = dp[i - 1][j];
-                    dp[i][j] = excProfit;
+                    // Cannot include item, so exclude it
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-
-        // Print DP table (as in image)
-        print(dp);
 
         // Final answer stored here
         return dp[n][W];
@@ -86,16 +57,16 @@ public class DP_8 {
 
     public static void main(String[] args) {
 
-        // Values array
+        // Values of items
         int val[] = {15, 14, 10, 45, 30};
 
-        // Weights array
+        // Weights of items
         int wt[] = {2, 5, 1, 3, 4};
 
-        // Knapsack capacity
+        // Capacity of knapsack
         int W = 7;
 
-        // Call tabulation function
-        System.out.println(knapsackTab(val, wt, W));
+        // Print maximum profit
+        System.out.println(knapsack01(val, wt, W));
     }
 }
